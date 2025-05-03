9a85508c258e7b6a05f4c341ea9a40e7d236c497
QIAKY <nathanqian@K-MacBook-Pro.local>
2025-05-03 15:13:10 +0930

    implemented receiver buffering and ACK handling

--- gbn.c
+++ gbn.c
@@ -231,60 +231,74 @@ void A_init(void)
 
 static int expectedseqnum; /* the sequence number expected next by the receiver */
 static int B_nextseqnum;   /* the sequence number for the next packets sent by B */
-
+static struct pkt B_buffer[SEQSPACE]; /*Cache received packets*/
+static bool B_received[SEQSPACE]; /*Mark whether the serial number has been received*/
 
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(struct pkt packet)
 {
-  struct pkt sendpkt;
+  struct pkt ackpkt; /*Define a packet for sending ACK*/
   int i;
+  int seq = packet.seqnum;/*The sequence number of the currently received data packet*/
 
-  /* if not corrupted and received packet is in order */
-  if  ( (!IsCorrupted(packet))  && (packet.seqnum == expectedseqnum) ) {
-    if (TRACE > 0)
-      printf("----B: packet %d is correctly received, send ACK!\n",packet.seqnum);
-    packets_received++;
+  /*If the received packet is not corrupted*/
+  if (!IsCorrupted(packet)) {
+    /*Debug information: print received packets*/
+    if (TRACE > 0){
+        printf("----B: received packet %d\n", seq);}
 
-    /* deliver to receiving application */
-    tolayer5(B, packet.payload);
+    /*If the packet with this sequence number has not been received before, cache it*/
+    if (!B_received[seq]) {
+        B_buffer[seq] = packet;  /*Store in buffer*/
+        B_received[seq] = true; /*Mark the serial number received*/
+    }
 
-    /* send an ACK for the received packet */
-    sendpkt.acknum = expectedseqnum;
+    /*Send ACK, confirm receipt of the sequence number*/
+    ackpkt.acknum = seq;  /*ACK confirms this packet*/
+    ackpkt.seqnum = 0; /*The ACK packet itself does not carry valid data, so seqnum can be set to 0*/
+    for (i = 0; i < 20; i++) /*Fill the ACK payload with the default value*/
+        ackpkt.payload[i] = '0';
+    ackpkt.checksum = ComputeChecksum(ackpkt);/*Calculate checksum*/
+    tolayer3(B, ackpkt); /*Send ACK back to sender A*/
 
-    /* update state variables */
-    expectedseqnum = (expectedseqnum + 1) % SEQSPACE;        
-  }
-  else {
-    /* packet is corrupted or out of order resend last ACK */
-    if (TRACE > 0) 
-      printf("----B: packet corrupted or not expected sequence number, resend ACK!\n");
-    if (expectedseqnum == 0)
-      sendpkt.acknum = SEQSPACE - 1;
-    else
-      sendpkt.acknum = expectedseqnum - 1;
-  }
 
-  /* create packet */
-  sendpkt.seqnum = B_nextseqnum;
-  B_nextseqnum = (B_nextseqnum + 1) % 2;
+    /*Print ACK send information*/
+    if (TRACE > 0){
+        printf("----B: ACK %d sent\n", seq);}
+
+    /*If the expected packets are received, they are delivered one by one*/
+    while (B_received[expectedseqnum]) {
+        tolayer5(B, B_buffer[expectedseqnum].payload); /*Delivered to the application layer*/
+        B_received[expectedseqnum] = false; /*clear mark*/
+        expectedseqnum = (expectedseqnum + 1) % SEQSPACE; /*Update expected sequence number*/
+    }
+
+} else {
+  /*If the packet is damaged, resend the last ACK*/
+    if (TRACE > 0){
+        printf("----B: packet corrupted, resend ACK %d\n", (expectedseqnum + SEQSPACE - 1) % SEQSPACE);}
     
-  /* we don't have any data to send.  fill payload with 0's */
-  for ( i=0; i<20 ; i++ ) 
-    sendpkt.payload[i] = '0';  
+    /*Construct the last successfully received ACK packet*/
+    ackpkt.acknum = (expectedseqnum + SEQSPACE - 1) % SEQSPACE;
+    ackpkt.seqnum = 0;
+    for (i = 0; i < 20; i++)
+        ackpkt.payload[i] = '0';
+    ackpkt.checksum = ComputeChecksum(ackpkt);
 
-  /* computer checksum */
-  sendpkt.checksum = ComputeChecksum(sendpkt); 
-
-  /* send out packet */
-  tolayer3 (B, sendpkt);
+    /*Send the ACK*/
+    tolayer3(B, ackpkt);
+  }
 }
 
 /* the following routine will be called once (only) before any other */
 /* entity B routines are called. You can use it to do any initialization */
 void B_init(void)
 {
-  expectedseqnum = 0;
-  B_nextseqnum = 1;
+  expectedseqnum = 0; /*Initialize the expected received sequence number to 0*/
+    /*Initialize the receive status array, indicating that all packets with sequence numbers have not been received*/
+    for (int i = 0; i < SEQSPACE; i++) {
+        B_received[i] = false;
+    }
 }
 
 /******************************************************************************
