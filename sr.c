5ff79def5796f2e445591a29fd8340951a43e952
QIAKY <nathanqian@K-MacBook-Pro.local>
2025-05-04 13:19:44 +0930

    Remove wrong timing

--- sr.c
+++ sr.c
@@ -75,8 +75,6 @@ static float current_time=0.0; /* Simulation time*/
 void A_output(struct msg message)
 { 
   int seq;
-  current_time = get_sim_time();
-
   /*If the current window is full, no new packets can be sent.*/
   if(windowcount>= WINDOWSIZE){
     if(TRACE >0){
@@ -117,7 +115,6 @@ void A_output(struct msg message)
 void A_input(struct pkt packet)
 {
   int acknum;
-  current_time = get_sim_time();
 
   /* If the received ACK is corrupted, it is ignored */
   if(IsCorrupted(packet)){
@@ -190,7 +187,6 @@ void A_input(struct pkt packet)
 void A_timerinterrupt(void)
 {
   int i;
-  current_time = get_sim_time();
   printf("          STOP TIMER: stopping timer at %.6f\n", current_time);
   /*Traverse the entire sequence number space, check which packets have timed out, and retransmit them one by one*/
   for (i=0; i<SEQSPACE; i++) {
@@ -250,7 +246,6 @@ void B_input(struct pkt packet)
   int i;
   int seq;
   seq = packet.seqnum;/*The sequence number of the currently received data packet*/
-  current_time = get_sim_time();
 
   /*If the received packet is not corrupted*/
   if (!IsCorrupted(packet)) {
